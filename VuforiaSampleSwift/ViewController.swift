5dd6832529f9b9f0a5f5b97f334994f1d342d27c
Asraful Alam <asraful.alam@mobioapp.com>
2017-09-24 16:15:24 +0600

    Initial Commit

---
+++ VuforiaSampleSwift/ViewController.swift
@@ -0,0 +1,169 @@
+//
+//  ViewController.swift
+//  VuforiaSample
+//
+//
+//  Created by Andrew Mendez on 2017/02/18.
+//  Copyright © 2017 Andrew Mendez. All rights reserved.
+//
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let vuforiaLicenseKey = "vuforiaLicenseKey.............."
+    let vuforiaDataSetFile = "StonesAndChips.xml"
+    
+    var vuforiaManager: VuforiaManager? = nil
+    var value = 0;
+    
+    let boxMaterial = SCNMaterial()
+    let boxMaterialC = SCNMaterial()
+    fileprivate var lastSceneName: String? = nil
+    fileprivate var middleSceneName: String? = nil
+    
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        prepare()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        do {
+            try vuforiaManager?.stop()
+        }catch let error {
+            print("\(error)")
+        }
+    }
+}
+
+private extension ViewController {
+    func prepare() {
+        vuforiaManager = VuforiaManager(licenseKey: vuforiaLicenseKey, dataSetFile: vuforiaDataSetFile)
+        if let manager = vuforiaManager {
+            
+            manager.delegate = self
+            // manager.eaglView.sceneSource = self
+            manager.eaglView.delegate = self
+            manager.eaglView.setupRenderer()
+            self.view = manager.eaglView
+        }
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(didRecieveWillResignActiveNotification),
+                                       name: NSNotification.Name.UIApplicationWillResignActive, object: nil)
+        
+        notificationCenter.addObserver(self, selector: #selector(didRecieveDidBecomeActiveNotification),
+                                       name: NSNotification.Name.UIApplicationDidBecomeActive, object: nil)
+        
+        vuforiaManager?.prepare(with: .portrait)
+    }
+    
+    func pause() {
+        do {
+            try vuforiaManager?.pause()
+        }catch let error {
+            print("\(error)")
+        }
+    }
+    
+    func resume() {
+        do {
+            try vuforiaManager?.resume()
+        }catch let error {
+            print("\(error)")
+        }
+    }
+}
+
+extension ViewController {
+    func didRecieveWillResignActiveNotification(_ notification: Notification) {
+        pause()
+    }
+    
+    func didRecieveDidBecomeActiveNotification(_ notification: Notification) {
+        resume()
+    }
+}
+
+extension ViewController: VuforiaManagerDelegate {
+    func vuforiaManagerDidFinishPreparing(_ manager: VuforiaManager!) {
+        print("did finish preparing\n")
+        
+        do {
+            try vuforiaManager?.start()
+            vuforiaManager?.setContinuousAutofocusEnabled(true)
+        }catch let error {
+            print("\(error)")
+        }
+    }
+    
+    func vuforiaManager(_ manager: VuforiaManager!, didFailToPreparingWithError error: Error!) {
+        print("did faid to preparing \(error)\n")
+    }
+    
+    func vuforiaManager(_ manager: VuforiaManager!, didUpdateWith state: VuforiaState!) {
+        for index in 0 ..< state.numberOfTrackableResults {
+            let result = state.trackableResult(at: index)
+            let trackerableName = result?.trackable.name
+            print("\(trackerableName)")
+            
+            
+            
+            if trackerableName == "stones" {
+                
+                if lastSceneName != "stones" {
+                    
+                    manager.eaglView.setStonesScene()
+                    
+                    lastSceneName = "stones"
+                    
+                }
+            }
+            
+            if trackerableName == "chips" {
+                
+                if middleSceneName != "chips" {
+                    
+                    manager.eaglView.setChipsScene()
+                    
+                    middleSceneName = "chips"
+                }
+            }
+        }
+    }
+}
+
+extension ViewController:  VuforiaEAGLViewDelegate {
+    
+    func vuforiaEAGLView(_ view: VuforiaEAGLView!, didTouchDownNode node: SCNNode!) {
+        print("touch down \(node.name)\n")
+        boxMaterial.transparency = 0.6
+    }
+    
+    func vuforiaEAGLView(_ view: VuforiaEAGLView!, didTouchUp node: SCNNode!) {
+        print("touch up \(node.name)\n")
+        boxMaterial.transparency = 1.0
+    }
+    
+    func vuforiaEAGLView(_ view: VuforiaEAGLView!, didTouchCancel node: SCNNode!) {
+        print("touch cancel \(node.name)\n")
+        boxMaterial.transparency = 1.0
+    }
+}
+
